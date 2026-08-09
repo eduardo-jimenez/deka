70d772076b63bc262d30187ac983a8d9efba5de6
Eduardo Jimenez <barkley@siesta-games.com>
2026-08-09 13:00:20 +0200

    added droplet ip to allowed hosts

--- generic-backend/backend/settings.py
+++ generic-backend/backend/settings.py
@@ -37,7 +37,7 @@ DEBUG = os.getenv("DEBUG", "False").lower() in {"1", "true", "yes", "on"}
 
 ALLOWED_HOSTS = get_env_list(
     "ALLOWED_HOSTS",
-    "localhost,127.0.0.1,deka-results-analyzer.onrender.com",
+    "localhost,127.0.0.1,165.232.106.17",
 )
 CSRF_TRUSTED_ORIGINS = get_env_list(
     "CSRF_TRUSTED_ORIGINS",
